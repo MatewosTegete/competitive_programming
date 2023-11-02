2c71656117490f6f4423a20a4bb49c3b17d6e191
MatewosTegete <131823681+MatewosTegete@users.noreply.github.com>
2023-11-02 18:20:58 +0400

    Add files via upload

---
+++ Boats to Save People.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        count = 0
+        i = 0
+        j = len(people) - 1
+        people.sort()
+        while i <= j:
+            if people[i] + people[j] <= limit:
+                i += 1
+            j -= 1
+            count += 1
+        return count
